4bc241858d58b356df8fd1c3a2b7902146a20085
oblaufuss <ocblaufuss@wpi.edu>
2020-08-31 22:31:28 -0400

    update accordion pages

---
+++ accordion/sidebar.js
@@ -0,0 +1,9 @@
+document.write('<a href="https://neocities.org/"><img src="https://bluef00t.neocities.org/imgs/neocities.gif"></a>');
+document.write('<br>');
+document.write('MY THINGS');
+document.write('<p><a href="art.html">art</a></p>');
+document.write('<p><a href="blinkies.html">blinkies</a></p>');
+document.write('<p><a href="whatprimedirective/index.html">game</a></p>');
+document.write('INTERESTS');
+document.write('<p><a href="comics/index.html">comics</a></p>');
+document.write('<p><a href="music/index.html">music</a></p>');
